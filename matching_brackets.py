e5ec61d0ec3e759867390a50c0046b21525be9f4
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-16 22:49:00 +0100

    Update matching_brackets.py

--- matching_brackets.py
+++ matching_brackets.py
@@ -2,28 +2,35 @@ def is_paired(input_string):
     br_open = 0
     sq_open = 0
     fl_open = 0
+    last = 0
     for ch in input_string:
         if (br_open > 0):
             if (ch == "]") or (ch == "}"):
                 return False
             elif (ch == ")"):
                 br_open = br_open -1
+                ch = " "
         if (sq_open > 0):
             if (ch == ")") or (ch == "}"):
                 return False
             elif (ch == "]"):
                 sq_open = sq_open -1
+                ch = " "
         if (fl_open > 0):
             if (ch == ")") or (ch == "]"):
                 return False
             elif (ch == "}"):
                 fl_open = fl_open -1
+                ch = " "
         if (ch == "("):
             br_open = br_open +1
+            last = 1
         elif (ch == "["):
             sq_open = sq_open +1
+            last = 2
         elif (ch == "{"):
             fl_open = fl_open +1
+            last = 3
     if (br_open == 0) and (sq_open == 0) and (fl_open == 0):
         return True
     else:
