d924f729632f06ea0024ac506fdfff1aac7dc436
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-30 22:13:23 +0100

    Update matching_brackets.py

--- matching_brackets.py
+++ matching_brackets.py
@@ -3,12 +3,16 @@
 
 def is_paired(input_string):
     test = []
+    mydict["("] = ")"
+    mydict["["] = "]"
+    mydict["{"] = "}"
     for ch in input_string:
         if (ch in "([{"):
             test.append(ch)
         if (ch in ")]}"):
             if len(test) >= 1:
-                if (test.pop() != ch):
+                last = test.pop()
+                if (mydict[last] != ch):
                     return False
             else:
                 return False
