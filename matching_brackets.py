4572660ff23caf2353a1121e86ed47e72e22e7b2
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-30 21:59:37 +0100

    Update matching_brackets.py

--- matching_brackets.py
+++ matching_brackets.py
@@ -4,26 +4,15 @@
 def is_paired(input_string):
     test = []
     for ch in input_string:
-        if (ch == "(") or (ch == "[") or (ch == "{"):
+        if (ch in "([{"):
             test.append(ch)
-        if len(test) >= 1:
-            if (ch == ")"):
-                if test[-1] == "(":
-                    del test[-1]
-                else:
+        if (ch in ")]}"):
+            if len(test) >= 1:
+                if (test.pop() != ch):
                     return False
-            elif (ch == "]"):
-                if test[-1] == "[":
-                    del test[-1]
-                else:
-                    return False
-            elif (ch == "}"):
-                if test[-1] == "{":
-                    del test[-1]
-                else:
-                    return False
-        elif (ch == ")") or (ch == "]") or (ch == "}"):
-            return False
+            else:
+                return False
+            
     if len(test) == 0:
         return True
     else:
